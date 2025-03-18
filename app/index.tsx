9e2229cb01d1daae03f650975e6a4f67a18f172c
jp kam <jpkamdem@proton.me>
2025-03-18 15:20:05 +0100

    grosse avancée

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,102 @@
-import { Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import boxesJson from "../assets/boxes.json";
+import { FlatList, Pressable, ScrollView } from "react-native-gesture-handler";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+
+type Aliment = {
+  nom: string;
+  quantite: number;
+};
+
+type Box = {
+  id: number;
+  nom: string;
+  pieces: number;
+  prix: number;
+  image: string;
+  aliments: Aliment[];
+  saveurs: string[];
+};
 
 export default function Index() {
+  const [boxes, setBoxes] = useState<Box[]>([]);
+  const [toggle, setToggle] = useState(false);
+
+  function filteredBoxes() {
+    const arr: Box[] = [];
+
+    boxes.map((box) => {
+      box.aliments.map((aliment) => {
+        if (aliment.nom.includes("Spring Saumon Avocat")) {
+          arr.push(box);
+        }
+      });
+    });
+
+    return arr;
+  }
+
+  useEffect(() => {
+    setBoxes(boxesJson);
+  }, []);
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <ScrollView>
+      <SafeAreaProvider>
+        <SafeAreaView>
+          {toggle ? (
+            <FlatList
+              data={boxes}
+              renderItem={(product) => (
+                <View key={product.item.id} id="all">
+                  <Text>{product.item.nom}</Text>
+                  <Text>
+                    Prix: {product.item.prix}€
+                  </Text>
+                  <Text>
+                    Liste des saveurs :{" "}
+                    {product.item.saveurs.map((item) => (
+                      <Text style={{ padding: 4 }}>{item}</Text>
+                    ))}
+                  </Text>
+                </View>
+              )}
+            />
+          ) : (
+            <FlatList
+              data={filteredBoxes()}
+              renderItem={(product) => (
+                <View key={product.item.id} id="saumon">
+                  <Text>{product.item.nom}</Text>
+                  <Text>
+                    Prix: {product.item.prix}€
+                  </Text>
+                  <Text>
+                    Saveurs :{" "}
+                    {product.item.saveurs.map((item) => (
+                      <Text style={{ padding: 4 }}>{item}</Text>
+                    ))}
+                  </Text>
+                </View>
+              )}
+            />
+          )}
+          <Pressable
+            onPress={() => setToggle(!toggle)}
+            style={{ padding: 4, backgroundColor: "red", width: 120 }}
+          >
+            <Text>
+              {toggle ? "Seulement les Spring Saumon Avocat" : "Afficher tout"}
+            </Text>
+          </Pressable>
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  hidden: {
+    display: "none",
+  },
+});
